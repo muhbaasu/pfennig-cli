2f64fc9f01829205bc459a5123654283740a250d
Samuel Schuhmacher <forgetaboutit@muhbaasu.de>
2016-03-29 23:42:11 +0200

    Move types to separate file

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,8 @@
 module Main where
 
-import           Control.Monad.Reader (runReaderT)
 import           Lib
-import qualified Options.Applicative  as OA
 
 main :: IO ()
 main = do
-  cmd <- OA.execParser (OA.info (OA.helper <*> programOpts) programInfo)
-  runReaderT (interpret cmd) defaultGlobalOptions
+  cmd <- parseArgs
+  runCommand cmd
